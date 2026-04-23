e2450d5b138810ce2f812419abd6484626506503
Ian <ozricsec@gmail.com>
2026-04-22 22:25:40 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,10 +8,15 @@ def main():
         client, _ = server_socket.accept()
         
         data = client.recv(4096).decode("utf-8")
+        print(data.split(" ")
         path = data.split(" ")[1]
+        print(path)
         
         if path == "/":
             client.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        if path.split("/")[0] == "echo":
+            body = path.split("/")[-1]
+            client.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body} \r\n\r\n".encode())
         else:
             client.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
     
