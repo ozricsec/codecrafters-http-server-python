1bbc9799554a0346f645145d5de6896f555c1946
Ian <ozricsec@gmail.com>
2026-04-22 22:29:13 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,13 +8,11 @@ def main():
         client, _ = server_socket.accept()
         
         data = client.recv(4096).decode("utf-8")
-        print(data.split(" "))
         path = data.split(" ")[1]
-        print(path.split("/"))
         
         if path == "/":
             client.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        if path.split("/")[0] == "echo":
+        if path.split("/")[1] == "echo":
             body = path.split("/")[-1]
             client.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body} \r\n\r\n".encode())
         else:
