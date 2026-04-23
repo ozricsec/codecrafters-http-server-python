b1c4909bc450749adc0db9abca8aba520c007471
Ian <ozricsec@gmail.com>
2026-04-23 02:14:35 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -100,6 +100,8 @@ async def client_handler(reader: asyncio.StreamReader, writer: asyncio.StreamWri
         elif parts[0] == "echo" and len(parts) > 1:
             if "gzip" in headers["accept-encoding"]:
                 handle_echo(path, True, writer)
+            else:
+                handle_echo(path, False, writer)
 
         elif parts[0] == "user-agent":
             handle_user_agent(headers, writer)
