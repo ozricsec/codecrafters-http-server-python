b92578cf818ce7917ee478ed617b3c26196f0fbb
Ian <ozricsec@gmail.com>
2026-04-22 22:52:26 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def main():
             body = path.split("/")[-1]
             client.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode())
         if path.split("/")[1] == "user-agent":
-            ua = headers[2][:12]
+            ua = headers[2][12:]
             print(ua)
             client.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(ua)}\r\n\r\n{ua}".encode())
         else:
